da7b7e1ffd430d5efcbbdb97c4e1058288efaf4d
Andrea Zaffignani <sicpoizn@gmail.com>
2019-05-13 15:28:08 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -51,36 +51,11 @@ function exclusion ($message_update, &$asset, &$direction, &$stoploss)
 	else return(-1);
 }
 
-$servername = "37.60.237.198";
-$username = "buddyzeu_andreaz";
-$password = "DGv-FeU-eEP-W7u";
-$dbname = "buddyzeu_licenze";
-$tablename = "FxMind_Builders_Signals";
 
 $prova_asset = "AUDUSD";
 $prova_dir = "OP_SELL";
 $prova_prezzo = 1.1010;
 
-// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) 
-	{
-		//die("Connection failed: " . $conn->connect_error);
-		$response = "Errore di Connessione al DB";
-	} 
-	
-	$sql = "INSERT INTO `".$tablename."`(`id_signal`, `asset`, `direction`, `stoploss`, `traded_flag`) 
-	VALUES (NULL,'$prova_asset','$prova_dir',$prova_prezzo,1)";
-
-
-	if ($conn->query($sql) === false) 
-	{
-		//$response = "Errore Query di Immissione Segnale nel DB";
-		$response = "Error: " . $sql . "<br>" . $conn->error;
-	} 
-
-	$conn->close();
 
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
@@ -109,6 +84,12 @@ $result = exclusion($text,$asset,$direction,$stoploss);
 
 if ($result == 1)
 {
+		$servername = "37.60.237.198";
+		$username = "buddyzeu_andreaz";
+		$password = "DGv-FeU-eEP-W7u";
+		$dbname = "buddyzeu_licenze";
+		$tablename = "FxMind_Builders_Signals";
+	
 	$response = "Segnale $direction\nAsset $asset\nStopLoss $stoploss";
 
 	// Create connection
