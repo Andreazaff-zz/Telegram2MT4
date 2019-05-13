720de7a91ec80b44183a4ca1f7f6919e7976e97f
Andrea Zaffignani <sicpoizn@gmail.com>
2019-05-13 13:46:50 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -81,38 +81,6 @@ if ($result == 1)
 }
 else $response = "Il segnale immesso non è valido.";
 
-/*
-if ($text == "ciao")
-{	
-	$response = "Ciao!!";
-}
-else
-{
-	if (strpos($string_exploded[0], "buy") == true) 
-	{
-   		$direction = "BUY";
-		$pos_dir = strpos($string_exploded[0], "buy");
-		$asset = substr($string_exploded[0],$pos_dir+4,6);
-		$entry_level = filter_var(substr($string_exploded[0],$pos_dir+4,strlen($string_exploded[0])-$pos_dir), FILTER_SANITIZE_NUMBER_FLOAT,FILTER_FLAG_ALLOW_FRACTION);
-		
-	}
-	if (strpos($string_exploded[0], "sell") == true) 
-	{
-   		$direction = "SELL";
-		$pos_dir = strpos($string_exploded[0], "sell");
-		$asset = substr($string_exploded[0],$pos_dir+5,6);
-		$entry_level = filter_var(substr($string_exploded[0],$pos_dir+5,strlen($string_exploded[0])-$pos_dir), FILTER_SANITIZE_NUMBER_FLOAT,FILTER_FLAG_ALLOW_FRACTION);
-		
-	}
-
-		$stoploss = filter_var(substr($string_exploded[1],0,10), FILTER_SANITIZE_NUMBER_FLOAT,FILTER_FLAG_ALLOW_FRACTION);
-		$tp1 = filter_var(substr($string_exploded[2],0,10), FILTER_SANITIZE_NUMBER_FLOAT,FILTER_FLAG_ALLOW_FRACTION);
-		$tp2 = filter_var(substr($string_exploded[1],0,10), FILTER_SANITIZE_NUMBER_FLOAT,FILTER_FLAG_ALLOW_FRACTION);
-		
-		$response = "Segnale $direction\nAsset $asset\nEntry Level $entry_level\nStopLoss $stoploss\nTake 1 $tp1\nTake2 $tp2";
-}
-*/
-
 header("Content-Type: application/json");
 $parameters = array('chat_id' => $chatId, "text" => $response);
 $parameters["method"] = "sendMessage";
